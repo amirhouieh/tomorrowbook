bfd8818bc9ef29e80e9b1a4704071bcf14ac6b91
amir houieh <amir.houieh@gmail.com>
2021-11-21 15:36:16 +0100

    fix grid layout on narrow screens

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -6,15 +6,15 @@ import Head from "next/head";
 export const Layout: React.FC<any> = ({children}) => (
     <>
         <Head>
-            <title>Tomorrow book</title>
-            <meta name="description" content="tomorrow book"/>
+            <title>TOOK</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="description" content="Took is an online reader generator that uses Wikipedia as its knowledge source."/>
             <link rel="icon" href="/favicon.ico"/>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true"}/>
             <link
                 href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=Roboto+Mono&family=Space+Mono&display=swap"
                 rel="stylesheet"/>
-
         </Head>
         <Navbar/>
         <main>{children}</main>
